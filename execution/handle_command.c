9a3e31e519422ec3a90d5dc6a170c1d689bdc2e1
Sanaa Zeroual <szeroual@c3r6p9.1337.ma>
2024-09-26 12:20:56 +0100

    sana

--- execution/handle_command.c
+++ execution/handle_command.c
@@ -1,39 +1,64 @@
 #include"mini.h"
-#include "../../parsing/minishell.h"
+#include "../minishell.h"
 
+void executing(t_ast *node, t_mini *box)
+{
+    if(node->type == COMMAND)
+    {
+        char **av = get_command(node);
+        if (!av)
+            return;
+        if (is_builtin(node)) 
+        {
+            int status = builtins(av, box, 0);
+            free(av);
+            exit(status);
+        }
+        else
+        {
+            char **command_path = get_path(box);
+            if (!command_path)
+            {
+                free(av);
+                return;
+            }
+            if (node->data->input_fd != STDIN_FILENO)
+            {
+                dup2(node->data->input_fd, STDIN_FILENO);
+                close(node->data->input_fd);
+            }
+            if (node->data->output_fd != STDOUT_FILENO)
+            {
+                dup2(node->data->output_fd, STDOUT_FILENO);
+                close(node->data->output_fd);
+            }
+            int i = 0;
+            char *full_path = NULL;
+            while (command_path[i])
+            {
+                char *temp = ft_strjoin(command_path[i], "/");
+                full_path = ft_strjoin(temp, av[0]);
+                free(temp);
 
-// void handle_command(t_ast *cmd, t_mini *box)
-// {
-//     char **argv = get_command(cmd);
-
-//     if (!argv)
-//         return;
-
-//     if (is_builtin(cmd))
-//     {
-//         int status = builtins(argv, box, 0);
-//         free(argv);
-//         exit(status);
-//     }
-//     else
-//     {
-//         char **path = get_path(box);
-//         if (!path)
-//         {
-//             free(argv);
-//             return;
-//         }
-        
-//         if (cmd->fd_in != STDIN_FILENO)
-//         {
-//             dup2(cmd->fd_in, STDIN_FILENO);
-//             close(cmd->fd_in);
-//         }
-//         if (cmd->fd_out != STDOUT_FILENO)
-//         {
-//             dup2(cmd->fd_out, STDOUT_FILENO);
-//             close(cmd->fd_out);
-//         }
-//         execute_with_path(argv, path, box);
-//     }
-// }
+                if (access(full_path, X_OK) == 0)
+                {
+                    execve(full_path, av, box->env);
+                    perror("execve error");
+                    exit(EXIT_FAILURE);
+                }
+                free(full_path);
+                i++;
+            }
+            perror("command not found");
+            free(av);
+            i = 0;
+            while (command_path[i])
+            {
+                free(command_path[i]);
+                i++;
+            }
+            free(command_path);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
