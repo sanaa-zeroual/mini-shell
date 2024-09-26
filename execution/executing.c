7d4c40a86ab3fdbb6c19ba9858cb921700ff886c
Sanaa Zeroual <szeroual@c3r6p9.1337.ma>
2024-09-26 15:48:15 +0100

    sa

--- execution/executing.c
+++ execution/executing.c
@@ -1,35 +1,35 @@
 #include "../minishell.h"
 
 
-void executing_(t_ast *cmd, t_pipe *pipefds)
-{
-    if(!cmd)
-        return;
-    if(cmd->type == PIPELINE)
-        execute_pipeline(cmd, pipefds);
-    else if(cmd->type == REDERECTION_IN || cmd->type == REDERECTION_HEREDOC)
-    {
-            redir_fd_in(cmd);
-            execute_command(cmd);
-    }
-    else if(cmd->type == REDERECTION_OUT || cmd->type == REDERECTION_APPEND)
-     {
-        redir_fd_out(cmd);
-        execute_command(cmd);
-     } 
-    else if(cmd->type == COMMAND)
-    {
-        char **av = get_command(cmd);
-        if(is_builtin(cmd->data->token->value))
-        {
-            builtins(av, NULL);
-            free(av);
-            exit(0);
-        }
-        else
-        {
-            char *path = get_path();
+// void executing_(t_ast *cmd, t_pipe *pipefds)
+// {
+//     if(!cmd)
+//         return;
+//     if(cmd->type == PIPELINE)
+//         execute_pipeline(cmd, pipefds);
+//     else if(cmd->type == REDERECTION_IN || cmd->type == REDERECTION_HEREDOC)
+//     {
+//             redir_fd_in(cmd);
+//             execute_command(cmd);
+//     }
+//     else if(cmd->type == REDERECTION_OUT || cmd->type == REDERECTION_APPEND)
+//      {
+//         redir_fd_out(cmd);
+//         execute_command(cmd);
+//      } 
+//     else if(cmd->type == COMMAND)
+//     {
+//         char **av = get_command(cmd);
+//         if(is_builtin(cmd->data->token->value))
+//         {
+//             builtins(av, NULL);
+//             free(av);
+//             exit(0);
+//         }
+//         else
+//         {
+//             char *path = get_path();
 
-        }
-    }
-}
+//         }
+//     }
+// }
