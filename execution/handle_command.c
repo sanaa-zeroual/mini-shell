ddcde3e6f70129b56abbcbcf8c2f551ff0fdc9b5
shebaz <Samia-Hb>
2024-10-22 21:40:15 +0000

    123

--- execution/handle_command.c
+++ execution/handle_command.c
@@ -4,81 +4,90 @@ void executing(t_ast *node, t_mini *box)
 {
     if (node->type == COMMAND)
     {
-        // printf("check here\n");
-        // int j = 0;
         if (!node->data->arguments)
-              return;
-        // while(node->data->arguments[j])
-        // {
-        //     printf("%s", node->data->arguments[j]);
-        //     j++;
-        // }
-        // printf("%s", node->data->arguments[0]);
-        if(is_builtin(node->data->token->value))
+            return;
+
+        if (is_builtin(node->data->token->value))
         {
-            int status = builtins(node->data->arguments, box);
-            exit(status);
+            builtins(node->data->arguments, box); // Execute built-in command
+            return; // Return to allow shell to continue running
         }
         else
         {
-            char **command_path = get_path();
-            if (!command_path)
-            {
-                free(node->data->arguments);
+            pid_t pid = fork(); // Create a child process
+
+            if (pid < 0) {
+                perror("Fork failed");
                 return;
             }
-            char **env_array = separate_env(box->env);
-            if (!env_array)
+
+            if (pid == 0) // Child process
             {
-                free(node->data->arguments);
-                free(command_path);
-                return;
-            }
-            if (node->data->input_fd != STDIN_FILENO)
-            {
-                dup2(node->data->input_fd, STDIN_FILENO);
-                close(node->data->input_fd);
-            }
-            if (node->data->output_fd != STDOUT_FILENO)
-            {
-                dup2(node->data->output_fd, STDOUT_FILENO);
-                close(node->data->output_fd);
-            }
-            int i = 0;
-            char *full_path = NULL;
-            while (command_path[i])
-            {
-                char *temp = ft_strjoin(command_path[i], "/");
-                if(!temp)
-                    return ;
-                full_path = ft_strjoin(temp, node->data->arguments[0]);
-                free(temp);
-                if (access(full_path, X_OK) == 0) 
-                {
-                    execve(full_path, node->data->arguments, env_array);
-                    perror("execve error");
-                    exit(EXIT_FAILURE); 
+                char **command_path = get_path();
+                if (!command_path) {
+                    free(node->data->arguments);
+                    exit(EXIT_FAILURE);
                 }
-                free(full_path);
-                i++;
+                char **env_array = separate_env(box->env);
+                if (!env_array) {
+                    free(node->data->arguments);
+                    free(command_path);
+                    exit(EXIT_FAILURE);
+                }
+
+                // Handle input and output redirection
+                if (node->data->input_fd != STDIN_FILENO) {
+                    dup2(node->data->input_fd, STDIN_FILENO);
+                    close(node->data->input_fd);
+                }
+                if (node->data->output_fd != STDOUT_FILENO) {
+                    dup2(node->data->output_fd, STDOUT_FILENO);
+                    close(node->data->output_fd);
+                }
+
+                // Attempt to execute the command
+                int i = 0;
+                char *full_path = NULL;
+                while (command_path[i]) {
+                    char *temp = ft_strjoin(command_path[i], "/");
+                    if (!temp) {
+                        free(node->data->arguments);
+                        exit(EXIT_FAILURE);
+                    }
+                    full_path = ft_strjoin(temp, node->data->arguments[0]);
+                    free(temp);
+                    if (access(full_path, X_OK) == 0) 
+                    {
+                        execve(full_path, node->data->arguments, env_array);
+                        perror("execve error"); // If execve fails
+                        exit(EXIT_FAILURE); 
+                    }
+                    free(full_path);
+                    i++;
+                }
+
+                perror("command not found");
+                free(node->data->arguments);
+                i = 0;
+                while (command_path[i]) {
+                    free(command_path[i]);
+                    i++;
+                }
+                free(command_path);
+                i = 0;
+                while (env_array[i]) {
+                    free(env_array[i]);
+                    i++;
+                }
+                free(env_array);
+                exit(EXIT_FAILURE); // Exit child process on error
             }
-            perror("command not found");
-            free(node->data->arguments);
-            i = 0;
-            while (command_path[i])
+            else // Parent process
             {
-                free(command_path[i]);
-                i++;
+                int status;
+                waitpid(pid, &status, 0); // Wait for child to finish
+                // Optionally handle status here
             }
-            free(command_path);
-            i = 0;
-            while (env_array[i])
-            {
-                free(env_array[i]);
-                i++;
-            }
-            free(env_array);
-            exit(EXIT_FAILURE);
         }
     }
 }
--- execution/postorde.c
+++ execution/postorde.c
@@ -1,5 +1,7 @@
 #include "../minishell.h"
 
+
+
 void algo_execution(t_ast *cmd, t_mini *box)
 {
     if (!cmd)
@@ -13,63 +15,111 @@ void algo_execution(t_ast *cmd, t_mini *box)
         exec_command_with_redirection(cmd);
     }
     else if (cmd->type == COMMAND)
-    {
-        exec_command_with_redirection(cmd);
-    }
+        executing(cmd,box);
     algo_execution(cmd->left, box);
     algo_execution(cmd->right, box);
 }
-
 // void algo_execution(t_ast *cmd, t_mini *box)
 // {
 //     if (!cmd)
 //         return;
-
-//     if (cmd->type == COMMAND) {
-//         // Handle input redirection
+//     // printf("cmd->value = %s\n", cmd->data->token->value);
+//     // exit(1);
+//     // printf("cmd->data->token = %s", cmd->data->token->expanded_value);
+//     // exit(1);
+//     if (cmd->type == COMMAND)
+//     {
+//         // printf("check\n");
+//         // exit(1);
 //         if (rfd_in(cmd) < 0)
 //             return;
-//         if (cmd->data->input_fd > 0) {
-//             dup2(cmd->data->input_fd, STDIN_FILENO);
-//             close(cmd->data->input_fd);
-//         }
-
-//         // Handle output redirection
 //         if (rfd_out(cmd) < 0)
 //             return;
-//         if (cmd->data->output_fd > 0) {
-//             dup2(cmd->data->output_fd, STDOUT_FILENO);
-//             close(cmd->data->output_fd);
-//         }
-
-//         // Execute the command
 //         executing(cmd, box);
 //     }
-//     else if (cmd->type == PIPELINE) {
+//     else if (cmd->type == PIPELINE)
+//     {
 //         execute_pipeline(cmd, box);
+//         printf("pipe");
+//         // exit(1);
 //     }
-//     else if (cmd->type == REDERECTION_OUT) {
-//         int saved_stdout = dup(STDOUT_FILENO);
-//         if (dup2(cmd->data->output_fd, STDOUT_FILENO) < 0) {
-//             perror("dup2 error");
-//             return;
+//     else if (cmd->type == REDERECTION_OUT)
+//     {
+//         // printf("heeey there\n");
+//         // // exit(1);
+//         // rfd_out(cmd);
+//         int saved_stdou = dup(STDOUT_FILENO);
+//         if(dup2(cmd->data->output_fd, STDOUT_FILENO) < 0)
+//         {
+//             printf("ERROR");
+//             return ;
 //         }
 //         algo_execution(cmd->left, box);
-//         dup2(saved_stdout, STDOUT_FILENO);
-//         close(saved_stdout);
+//         dup2(saved_stdou, STDOUT_FILENO);
+//         close(saved_stdou);
 //     }
-//     else if (cmd->type == REDERECTION_IN) {
-//         int saved_stdin = dup(STDIN_FILENO);
-//         if (dup2(cmd->data->input_fd, STDIN_FILENO) < 0) {
-//             perror("dup2 error");
-//             return;
-//         }
-//         algo_execution(cmd->left, box);
-//         dup2(saved_stdin, STDIN_FILENO);
-//         close(saved_stdin);
-//     }
-
-//     // Recursively execute the left and right children of the AST
-//     algo_execution(cmd->left, box);
+//     // else if(cmd->type == REDERECTION_IN)
+//     // {
+//     //     // printf("hy in");
+//     //     // exit(1);
+//     //     // rfd_in(cmd);
+//     //     algo_execution(cmd->left, box);
+//     //     // exec_cmd(cmd, box);
+//     // }
 //     algo_execution(cmd->right, box);
+//     algo_execution(cmd->left, box);
 // }
+
+// // void algo_execution(t_ast *cmd, t_mini *box)
+// // {
+// //     if (!cmd)
+// //         return;
+
+// //     if (cmd->type == COMMAND) {
+// //         // Handle input redirection
+// //         if (rfd_in(cmd) < 0)
+// //             return;
+// //         if (cmd->data->input_fd > 0) {
+// //             dup2(cmd->data->input_fd, STDIN_FILENO);
+// //             close(cmd->data->input_fd);
+// //         }
+
+// //         // Handle output redirection
+// //         if (rfd_out(cmd) < 0)
+// //             return;
+// //         if (cmd->data->output_fd > 0) {
+// //             dup2(cmd->data->output_fd, STDOUT_FILENO);
+// //             close(cmd->data->output_fd);
+// //         }
+
+// //         // Execute the command
+// //         executing(cmd, box);
+// //     }
+// //     else if (cmd->type == PIPELINE) {
+// //         execute_pipeline(cmd, box);
+// //     }
+// //     else if (cmd->type == REDERECTION_OUT) {
+// //         int saved_stdout = dup(STDOUT_FILENO);
+// //         if (dup2(cmd->data->output_fd, STDOUT_FILENO) < 0) {
+// //             perror("dup2 error");
+// //             return;
+// //         }
+// //         algo_execution(cmd->left, box);
+// //         dup2(saved_stdout, STDOUT_FILENO);
+// //         close(saved_stdout);
+// //     }
+// //     else if (cmd->type == REDERECTION_IN) {
+// //         int saved_stdin = dup(STDIN_FILENO);
+// //         if (dup2(cmd->data->input_fd, STDIN_FILENO) < 0) {
+// //             perror("dup2 error");
+// //             return;
+// //         }
+// //         algo_execution(cmd->left, box);
+// //         dup2(saved_stdin, STDIN_FILENO);
+// //         close(saved_stdin);
+// //     }
+
+// //     // Recursively execute the left and right children of the AST
+// //     algo_execution(cmd->left, box);
+// //     algo_execution(cmd->right, box);
+// // }
